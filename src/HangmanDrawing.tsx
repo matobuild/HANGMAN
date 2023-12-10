6775cd7a5ae050dc71692678844149a1dc39b538
Mato <Kittawat@icloud.com>
2023-12-10 15:38:25 +0800

    finish

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -83,15 +83,16 @@ const LEFT_LEG = (
     }}
   ></div>
 );
-export function HangmanDrawing() {
+
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
+
+type HangmanDrawingProp = {
+  numberOfGuesses: number;
+};
+export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProp) {
   return (
     <div style={{ position: "relative" }}>
-      {HEAD}
-      {BODY}
-      {RIGHT_ARM}
-      {LEFT_ARM}
-      {RIGHT_LEG}
-      {LEFT_LEG}
+      {BODY_PARTS.slice(0, numberOfGuesses)}
       <div
         style={{
           height: "50px",
--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,1 +1,38 @@
-export function HangmanWord() {}
+type HangmanWordProps = {
+  guessedLetter: string[];
+  wordToGuess: string;
+  reveal?: boolean;
+};
+export function HangmanWord({
+  guessedLetter,
+  wordToGuess,
+  reveal = false,
+}: HangmanWordProps) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25rem",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {wordToGuess.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility:
+                guessedLetter.includes(letter) || reveal ? "visible" : "hidden",
+              color:
+                !guessedLetter.includes(letter) && reveal ? "red" : "black",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
+}
